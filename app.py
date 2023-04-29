cd6a4b12578e3037ae456f229e998895f20caf86
Ah_bb <ahabbscience@gmail.com>
2023-04-30 00:17:23 +0500

    Add files via upload
    Basic admin portal created and 75pc working stripe integration.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, flash,render_template,request,session, redirect, url_for
+from flask import Flask, flash,render_template,request,session, redirect, url_for, abort
 from strgen import StringGenerator as SG
 from sqlalchemy import create_engine  
 from sqlalchemy import Column, String  
@@ -7,14 +7,30 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import joinedload
 import uuid
-from flask import Flask
 import logging
+from werkzeug.utils import secure_filename
+import os
+from dotenv import load_dotenv
+import requests
+import stripe
+
+load_dotenv()
+
+#stripe_publishable_key = os.getenv('STRIPE_PUBLISHABLE_KEY')
+#stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
+stripe_public_key = "sk_test_51N1tPbGZpL6j0pjIrNDvGgTBvQQUCjgGiDS55f6Ah48ZrlUKJuh4tkVheRkq7pokKS3ngfAL9Ioa1z5mwq6UEjqW00XTe0nc78"
+stripe.api_key = "sk_test_51N1tPbGZpL6j0pjIrNDvGgTBvQQUCjgGiDS55f6Ah48ZrlUKJuh4tkVheRkq7pokKS3ngfAL9Ioa1z5mwq6UEjqW00XTe0nc78"
 
 
 
 app = Flask(__name__)
 
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'jpg', 'jpeg', 'png', 'gif'}
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 #configure app to enable it to interact with your database
 #format: 'postgresql://user:password@localhost/database name'
@@ -143,6 +159,7 @@ class Product(db.Model):
     product_id = db.Column(db.String(20), primary_key=True)
     quantity_pu = db.Column(db.Integer)
     product_name = db.Column(db.String(20))
+    product_image = db.Column(db.String)
     stock_units = db.Column(db.Integer)
     unit_weight = db.Column(db.Float(53))
     discount = db.Column(db.Float(53))
@@ -390,10 +407,179 @@ def index():
 
     return render_template('index.html', productData=productData, categoryData=categoryData)
 
+@app.route('/checkout', methods=['GET', 'POST'])
+def checkout():
+    if 'customer_id' not in session:
+        flash('Please login first', 'warning')
+        return redirect(url_for('login'))
+    
+    cust_id = session['customer_id']
+    cart = Cart.query.filter_by(customer=cust_id).first()
+    total_price = cart.total_price
+    nop = cart.nop
+    products = (
+        db.session.query(Product, CartProduct.quantity)
+        .join(CartProduct)
+        .join(Cart)
+        .filter(Cart.customer == cust_id)
+        .all()
+        )
+    
+    if request.method == 'POST':
+        cart_products = CartProduct.query.filter_by(cart_id=cart.cart_id).all()
+        line_items = []
+        for cart_product in cart_products:
+            product = Product.query.get(cart_product.product_id)
+            line_items.append({
+                'price_data': {
+                    'currency': 'usd',
+                    'product_data': {
+                        'name': product.product_name,
+                        'images': [product.product_image],
+                    },
+                    'unit_amount': product.price * cart_product.quantity * 100,
+                },
+                'quantity': cart_product.quantity,
+            })
+
+        session['line_items'] = line_items
+        session['description'] = 'Order from Kuchu Muchu'
+        session['amount'] = total_price * 100
+        session['nop'] = nop
+        session['cart_id'] = cart.cart_id
+        session['customer_email'] = Customer.query.get(cust_id).loginid
+        
+        checkout_session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=line_items,
+            mode='payment',
+            success_url=url_for('success', _external=True),
+            cancel_url=url_for('cancel', _external=True),
+        )
+        
+        return redirect(checkout_session.url, code=303)
+    
+    return render_template('checkout.html', totalPrice=total_price, noOfItems=nop, stripe_public_key=os.environ.get('STRIPE_PUBLISHABLE_KEY'), cart_items = products)
+
+@app.route('/success')
+def success():
+    description = session['description']
+    amount = session['amount']
+    nop = session['nop']
+    cart_id = session['cart_id']
+    cust_id = session['customer_id']
+    
+    cart = Cart.query.filter_by(cart_id=cart_id).first()
+    cart.nop = nop
+    cart.total_price = amount
+    db.session.commit()
+    
+    order = Orderdetail(unit_price=amount, discount=0, order_num=1, quantity=nop)
+    db.session.add(order)
+    db.session.commit()
+    
+    # Clear the cart
+    cart_products = CartProduct.query.filter_by(cart_id=cart_id).all()
+    for cp in cart_products:
+        db.session.delete(cp)
+    db.session.commit()
+    
+    return render_template('success.html', description=description, amount=amount, nop=nop)
+
+@app.route('/cancel')
+def cancel():
+    return render_template('cancel.html')
+
+@app.route('/webhook', methods=['POST'])
+def webhook_received():
+    payload = request.get_data(as_text=True)
+    sig_header = request.headers.get('Stripe-Signature')
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, os.environ['STRIPE_ENDPOINT_SECRET']
+        )
+    except ValueError as e:
+        return str(e), 400
+    except stripe.error.SignatureVerificationError as e:
+        return str(e), 400
+
+    # Process the event
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+        # Handle successful payment (e.g., fulfill the order)
+    
+    return '', 200
+
+
 @app.route('/logout')
 def logout():
     session.pop('username', None)
     return redirect(url_for('login'))
+
+
+
+#routing the app to the login page.(In this case this is my starting page for now)
+@app.route('/admin', methods=['GET', 'POST'])
+def admin_login():
+    if request.method == 'GET':
+         return render_template('admin_login.html')
+    
+    # Verify reCAPTCHA
+    # response = request.form.get('g-recaptcha-response')
+    # if not response:
+    #     return render_template('login.html', error='Please complete the reCAPTCHA.')
+    
+    admin_id = request.form.get('admin_id')
+    entered_password = request.form.get('password')
+
+    if admin_id and entered_password:
+        admin = Admin.query.filter_by(admin_id=admin_id).first()
+        if admin and admin.login_id == entered_password:
+            session['admin_id'] = admin.admin_id
+            return render_template('admin_dashboard.html')
+        else:
+            error = 'Invalid Credentials. Please try again.'
+    else:
+        error = 'Missing username or password. Please try again.'
+    return render_template('admin_login.html', error=error)
+
+
+@app.route('/add_product', methods=['GET', 'POST'])
+def add_product():
+    if 'admin_id' not in session:
+        return redirect(url_for('admin_login'))
+    
+    if request.method == 'POST':
+        name = request.form['name']
+        description = request.form['description']
+        price = request.form['price']
+        category = request.form['category']
+        discount = request.form['discount']
+        quantity = request.form['quantity']
+
+        # Check if product already exists
+        existing_product = Product.query.filter_by(product_name=name, product_description=description, price=price, category=category, discount=discount).first()
+        if existing_product:
+            # Return an error message if the product already exists
+            return render_template('admin_dashboard.html', message="Product already exists")
+
+        # save uploaded image file
+        if 'image' in request.files:
+            image_file = request.files['image']
+            if image_file and allowed_file(image_file.filename):
+                filename = secure_filename(image_file.filename)
+                image_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        new_product = Product(product_id = str(uuid.uuid4())[:20], product_name=name, product_description=description, price=price, quantity_pu = quantity, product_image = filename, category = category, discount = discount)
+        db.session.add(new_product)
+        db.session.commit()
+
+        return redirect(url_for('admin'))
+
+    return render_template('admin_dashboard.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
